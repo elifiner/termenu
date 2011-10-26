450f150edc105a95270b71a667696e8cbef3baba
Eli Golovinsky <elig@il.ibm.com>
2011-10-26 23:31:14 +0200

    make termenu work with redirected input and output and convert it into a bash usable script

--- termenu.py
+++ termenu.py
@@ -1,14 +1,13 @@
+#!/usr/bin/python
 import sys
 import ansi
-import keyboard
 
 # Get the size of the current terminal
 # (http://stackoverflow.com/questions/566746/how-to-get-console-window-width-in-python)
 def get_terminal_size():
     import fcntl, termios, struct
-    h, w, hp, wp = struct.unpack('HHHH',
-        fcntl.ioctl(0, termios.TIOCGWINSZ,
-        struct.pack('HHHH', 0, 0, 0, 0)))
+    h, w, hp, wp = struct.unpack('HHHH', fcntl.ioctl(sys.stdin,
+        termios.TIOCGWINSZ, struct.pack('HHHH', 0, 0, 0, 0)))
     return w, h
 
 class Menu(object):
@@ -22,13 +21,19 @@ class Menu(object):
     """
 
     MARGIN = 5
-    def __init__(self, header, options, default=0, clearOnExit=True, separator="  "):
-        self.header = header
+    def __init__(self, title, options, default=None, clearOnExit=True, separator="  "):
+        self.title = title
         self.options = list(options)
-        self.default = default
+        if default is None:
+            default = 0
+        elif isinstance(default, str):
+            try:
+                default = options.index(default)
+            except ValueError:
+                default = 0
         self.clearOnExit = clearOnExit
         self.separator = separator
-        self.selected = max(0, self.default % len(self.options))
+        self.selected = max(0, default % len(self.options))
         self.slices = lambda: self._paginate(options, get_terminal_size()[0])
 
     def _print(self, data):
@@ -41,12 +46,12 @@ class Menu(object):
         optionsCopy[self.selected] = ansi.colorize(optionsCopy[self.selected], "black", "white")
         optionsStr = self.separator.join(optionsCopy[first:last])
         if first > 0:
-            optionsStr = "< " + optionsStr
+            optionsStr = ansi.colorize("< ", "white", bright=True) + optionsStr
         if last < len(self.options):
-            optionsStr = optionsStr + " >"
+            optionsStr = optionsStr + ansi.colorize(" >", "white", bright=True)
         ansi.clear_line()
         self._print("\r")
-        self._print(ansi.colorize(self.header, "white", bright=True) + optionsStr)
+        self._print(ansi.colorize(self.title, "white", bright=True) + optionsStr)
 
     def _paginate(self, options, maxWidth):
         slices = []
@@ -54,7 +59,7 @@ class Menu(object):
         contents = []
         for i, option in enumerate(options):
             contents.append(option)
-            if len(self.header) + len(self.separator.join(contents)) + self.MARGIN > maxWidth:
+            if len(self.title) + len(self.separator.join(contents)) + self.MARGIN > maxWidth:
                 slices.append((start, i))
                 start = i
                 contents = [option]
@@ -66,14 +71,17 @@ class Menu(object):
         Show the menu and run the keyboard loop. The return value is the text of the chosen option
         if Enter was pressed and None is Esc was pressed.
         """
+        import keyboard
         ansi.hide_cursor()
         try:
             self._print_menu()
             for key in keyboard.keyboard_listener():
                 if key == "right":
-                    self.selected = (self.selected + 1) % len(self.options)
+                    if self.selected < len(self.options)-1:
+                        self.selected += 1
                 elif key == "left":
-                    self.selected = (self.selected + len(self.options) - 1) % len(self.options)
+                    if self.selected > 0:
+                        self.selected -= 1
                 elif key == "enter":
                     return self.options[self.selected]
                 elif key == "esc":
@@ -96,9 +104,16 @@ class VerticalMenu(object):
         result = menu.show()
         print result
     """
-    def __init__(self, header, options, default=0, clearOnExit=True, height=None):
-        self.header = header
+    def __init__(self, title, options, default=None, clearOnExit=True, height=None):
+        self.title = title
         self.options = options
+        if default is None:
+            default = 0
+        elif isinstance(default, str):
+            try:
+                default = options.index(default)
+            except ValueError:
+                default = 0
         self.selected = max(0, default % len(self.options))
         self.width = max(len(option) for option in self.options)
         self.clearOnExit = clearOnExit
@@ -136,7 +151,9 @@ class VerticalMenu(object):
         Show the menu and run the keyboard loop. The return value is the text of the chosen option
         if Enter was pressed and None is Esc was pressed.
         """
-        self._print(ansi.colorize(self.header, "white", bright=True) + "\n")
+        import keyboard
+        if self.title:
+            self._print(ansi.colorize(self.title, "white", bright=True) + "\n")
         self._print_menu(False)
         ansi.save_position() # save bottom-of-menu position for future reference
         ansi.hide_cursor()
@@ -173,23 +190,84 @@ class VerticalMenu(object):
         finally:
             if self.clearOnExit:
                 ansi.restore_position()
-                ansi.up(self.height+1)
-                for i in xrange(self.height+1):
+                lines = self.height
+                if self.title:
+                    lines += 1
+                ansi.up(lines)
+                for i in xrange(lines):
                     ansi.clear_line()
                     ansi.down()
-                ansi.up(self.height+1)
+                ansi.up(lines)
             ansi.show_cursor()
 
-def show_menu(header, options, default=0, clearOnExit=True):
-    menu = Menu(header, options, default, clearOnExit)
+def show_menu(title, options, default=None, clearOnExit=True):
+    menu = Menu(title, options, default, clearOnExit)
     return menu.show()
 
-def show_vertical_menu(header, options, default=0, clearOnExit=True, height=None):
-    menu = VerticalMenu(header, options, default, clearOnExit, height)
+def show_vertical_menu(title, options, default=None, clearOnExit=True, height=None):
+    menu = VerticalMenu(title, options, default, clearOnExit, height)
     return menu.show()
     
+def main():
+    # Always connect stdin/stdout to the controlling terminal, even if redirected
+    redirectedStdin = sys.stdin
+    redirectedStdout = sys.stdout
+    if not sys.stdin.isatty():
+        sys.stdin = open("/dev/tty")
+    if not sys.stdout.isatty():
+        sys.stdout = open("/dev/tty", "w")
+
+    from optparse import OptionParser, IndentedHelpFormatter
+    class MyHelpFormatter(IndentedHelpFormatter):
+        def format_description(self, description):
+            return description
+
+    description = """\
+Shows an inline interactive menu. Menu items can be supplied as arguments,
+via STDIN (if no arguments were given) or a file (using -f).
+Menus can be vertical (multi-line) or one-line.
+
+Examples:
+    termenu.py Abort Retry Fail
+    ls | termenu.py -
+    termenu.py -f file_with_options.txt
+"""
+    parser = OptionParser(usage="Usage: %prog [items]", description=description, formatter=MyHelpFormatter(), add_help_option=False)
+    parser.add_option("--help", dest="help", help="Show help message", action="store_true", default=False)
+    parser.add_option("-f", "--file", dest="file", help="Take menu items from a file", metavar="FILE")
+    parser.add_option("-v", "--vertical", dest="vertical", help="Display a vertical menu", action="store_true", default=True)
+    parser.add_option("-h", "--horizontal", dest="vertical", help="Display a horizontal menu", action="store_false")
+    parser.add_option("-t", "--title", dest="title", help="A title for the menu", default="")
+    parser.add_option("-d", "--default", dest="default", help="Default item to select", metavar="OPTION")
+    parser.add_option("-l", "--lines", dest="lines", type="int", help="Max lines for vertical menu", metavar="LINES", default=20)
+    (options, args) = parser.parse_args()
+
+    if options.help:
+        parser.print_help()
+        sys.exit(255)
+
+    items = []
+
+    try:
+        if len(args) == 0:
+            items = [l.strip() for l in redirectedStdin.readlines()]
+        elif options.file:
+            items = open(options.file).readlines()
+        else:
+            items = args
+    except IOError, e:
+        parser.error(str(e))
+
+    if not items:
+        parser.error("no menu items provided")
+
+    if options.vertical:
+        result = show_vertical_menu(options.title, items, default=options.default, height=options.lines)
+    else:
+        result = show_menu(options.title, items, default=options.default)
+
+    if result:
+        redirectedStdout.write(result + "\n")
+
 if __name__ == "__main__":
-    import __builtin__
-    builtin = show_vertical_menu("Show help for: ", sorted(dir(__builtin__), key=lambda v: v.lower()), clearOnExit=True, height=20)
-    if builtin:
-        help(builtin)
+    main()
