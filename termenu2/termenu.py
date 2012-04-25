99d3e1d8846613d08970b1378b758cc8cf0df667
Eli Golovinsky <elig@il.ibm.com>
2012-04-25 17:46:56 +0300

    added multiselection

--- termenu2/termenu.py
+++ termenu2/termenu.py
@@ -9,12 +9,23 @@ class Termenu(object):
         self.lines = lines     # number of lines visible on screen
         self.cursor = 0        # visible cursor position (0 is top visible option)
         self.scroll = 0        # index of first visible option
+        self.selected = set()
+        self._maxOptionLen = max(len(o) for o in self.options)
 
     def get_visible_lines(self):
         return self.options[self.scroll:self.scroll+self.lines]
 
-    def get_selected(self):
+    def get_active(self):
         return self.options[self.scroll+self.cursor]
+
+    def get_selected(self):
+        return [o for i, o in enumerate(self.options) if i in self.selected] 
+
+    def get_result(self):
+        if self.selected:
+            return self.get_selected()
+        else:
+            return [self.get_active()]
 
     def on_down(self):
         if self.cursor < self.lines - 1:
@@ -44,18 +55,47 @@ class Termenu(object):
         else:
             self.scroll = 0
 
-    def on_enter(self):
-        return True
-
-    def on_esc(self):
-        return True
+    def on_space(self):
+        index = self.scroll + self.cursor
+        if index in self.selected:
+            self.selected.remove(index)
+        else:
+            self.selected.add(index)
+        self.on_down()
 
     def print_menu(self):
         for i, line in enumerate(self.get_visible_lines()):
-            if self.cursor == i:
-                print "*" + line
-            else:
-                print " " + line
+            line = self.decorate(line, 
+                active = (self.cursor == i), 
+                selected = (self.scroll+i in self.selected),
+                moreAbove = (self.scroll > 0 and i == 0),
+                moreBelow = (self.scroll + self.lines < len(self.options) and i == self.lines - 1),
+            )
+            print line
+
+    def decorate(self, line, active=False, selected=False, moreAbove=False, moreBelow=False):
+        # all lines to same width
+        line = "{0:<{width}}".format(line, width=self._maxOptionLen)
+
+        # add selection / cursor decorations
+        if active and selected:
+            line = "*" + ansi.colorize(line, "red", "white")
+        elif active:
+            line = " " + ansi.colorize(line, "black", "white")
+        elif selected:
+            line = "*" + ansi.colorize(line, "red")
+        else:
+            line = " " + line
+
+        # add more above/below indicators
+        if moreAbove:
+            line = line + ansi.colorize("^", "white", bright=True)
+        elif moreBelow:
+            line = line + ansi.colorize("v", "white", bright=True)
+        else:
+            line = line + " "
+
+        return line
 
     def clear_menu(self):
         ansi.restore_position()
@@ -72,20 +112,22 @@ class Termenu(object):
     def show(self):
         import keyboard
         self.print_menu()
-        ansi.save_position() # save bottom-of-menu position for future reference
+        ansi.save_position()
         ansi.hide_cursor()
         try:
             for key in keyboard.keyboard_listener():
-                ret = self.dispatch_key(key)
-                if ret:
-                    return self.get_selected()
-                ansi.restore_position() # go to bottom of menu
-                ansi.up(self.lines) # go to top of menu
+                self.dispatch_key(key)
+                if key == "enter":
+                    return self.get_result()
+                elif key == "esc":
+                    return None
+                ansi.restore_position()
+                ansi.up(self.lines)
                 self.print_menu()
         finally:
             self.clear_menu()
             ansi.show_cursor()
 
 if __name__ == "__main__":
-    menu = Termenu(["option-%06d" % i for i in xrange(1,100)], lines=5)
+    menu = Termenu(["option-%06d" % i for i in xrange(1,100)], lines=10)
     print menu.show()
--- termenu2/test.py
+++ termenu2/test.py
@@ -1,11 +1,14 @@
+import sys
+sys.path.append("..")
 import unittest
+import ansi
 from termenu import Termenu
 
 OPTIONS = ["%02d" % i for i in xrange(1,100)]
 
 def strmenu(menu):
     s = " ".join(menu.get_visible_lines())
-    s = s.replace(menu.get_selected(), "(%s)" % menu.get_selected())
+    s = s.replace(menu.get_active(), "(%s)" % menu.get_active())
     return s
 
 class Down(unittest.TestCase):
@@ -138,6 +141,63 @@ class PageUp(unittest.TestCase):
         assert strmenu(menu) == "(02) 03 04 05"
         menu.on_pageUp()
         assert strmenu(menu) == "(01) 02 03 04"
+
+class MultiSelect(unittest.TestCase):
+    def test_select(self):
+        menu = Termenu(OPTIONS, lines=4)
+        assert strmenu(menu) == "(01) 02 03 04"
+        menu.on_space()
+        menu.on_space()
+        assert strmenu(menu) == "01 02 (03) 04"
+        assert " ".join(menu.get_selected()) == "01 02"
+        assert " ".join(menu.get_result()) == "01 02"
+
+    def test_deselect(self):
+        menu = Termenu(OPTIONS, lines=4)
+        assert strmenu(menu) == "(01) 02 03 04"
+        menu.on_space()
+        menu.on_up()
+        menu.on_space()
+        assert strmenu(menu) == "01 (02) 03 04"
+        assert " ".join(menu.get_selected()) == ""
+        assert " ".join(menu.get_result()) == "02"
+
+def active(s):
+    return ansi.colorize(s, "black", "white")
+
+def selected(s):
+    return ansi.colorize(s, "red")
+
+def active_selected(s):
+    return ansi.colorize(s, "red", "white")
+
+def white(s):
+    return ansi.colorize(s, "white", bright=True)
+
+class Decorate(unittest.TestCase):
+    def test_active(self):
+        menu = Termenu(OPTIONS, lines=4)
+        assert menu.decorate("text", active=True) == " " + active("text") + " "
+
+    def test_selected(self):
+        menu = Termenu(OPTIONS, lines=4)
+        assert menu.decorate("text", selected=True) == "*" + selected("text") + " "
+
+    def test_active_selected(self):
+        menu = Termenu(OPTIONS, lines=4)
+        assert menu.decorate("text", active=True, selected=True) == "*" + active_selected("text") + " "
+
+    def test_more_above(self):
+        menu = Termenu(OPTIONS, lines=4)
+        assert menu.decorate("text", active=True, selected=True, moreAbove=True) == "*" + active_selected("text") + white("^")
+
+    def test_more_below(self):
+        menu = Termenu(OPTIONS, lines=4)
+        assert menu.decorate("text", active=True, selected=True, moreBelow=True) == "*" + active_selected("text") + white("v")
+
+    def test_max_option_len(self):
+        menu = Termenu("one three fifteen twenty eleven".split(), lines=4)
+        assert menu.decorate("three", active=True, selected=True) == "*" + active_selected("three  ") + " "
 
 if __name__ == "__main__":
     unittest.main()
