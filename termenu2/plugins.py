f8f551efd39cf1a81ce6a78b103ae4e7e43f91d3
Eli Golovinsky <elig@il.ibm.com>
2012-08-23 14:27:57 +0300

    wip

--- termenu2/plugins.py
+++ termenu2/plugins.py
@@ -1,23 +1,41 @@
 def pluggable(method):
+    """
+    Marks a method to be extendable via plugins.
+    When the method is called, the last installed plugin will be called and the 
+    call will propagate up the stack of plugins until the original method is called.
+    """
     def wrapped(self, *args, **kwargs):
-        stack = [method]
-        for plugin in self.plugins:
-            stack.append(getattr(plugin, method.__name__))
+        stack = [lambda *args, **kwargs: method(self, *args, **kwargs)]
+        for plugin in self._plugins:
+            if hasattr(plugin, method.__name__):
+                stack.append(getattr(plugin, method.__name__))
         return call_previous(stack, *args, **kwargs)
     return wrapped
 
 def call_previous(stack, *args, **kwargs):
-    return stack[-1](stack[:-1], *args, **kwargs)
+    """
+    Calls the previous plugin in the plugin chain.
+    """
+    method = stack.pop()
+    if stack:
+        return method(stack, *args, **kwargs)
+    else:
+        return method(*args, **kwargs)
 
 class Plugin(object):
     pass
 
-class Termenu(object):
-    def __init__(self, plugins):
-        self.plugins = plugins
+class Server(object):
+    def register_plugin(self, plugin):
+        if not hasattr(self, "_plugins"):
+            self._plugins = []
+        self._plugins.append(plugin)
 
+# ----------------------------------------------------------------------
+
+class SampleServer(Server):
     @pluggable
-    def dummy(self, param):
+    def foo(self, param):
         print "original %s" % param
         return 7
 
@@ -25,7 +43,7 @@ class SamplePlugin(Plugin):
     def __init__(self, name):
         self.name = name
 
-    def dummy(self, stack, param):
+    def foo(self, stack, param):
         print "%s enter %s" % (self.name, param)
         result = call_previous(stack, param)
         print "%s enter %s" % (self.name, param)
@@ -34,5 +52,7 @@ class SamplePlugin(Plugin):
 if __name__ == "__main__":
     plugin1 = SamplePlugin("sample1")
     plugin2 = SamplePlugin("sample2")
-    menu = Termenu([plugin1, plugin2])
-    print menu.dummy("momo")
+    server = SampleServer()
+    server.register_plugin(plugin1)
+    server.register_plugin(plugin2)
+    print server.foo("momo")
