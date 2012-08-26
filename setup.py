175f817b6546f5dc4f7af03255ff27a6f5aa1168
Eli Golovinsky <elig@il.ibm.com>
2012-08-26 19:00:11 +0300

    changed my name

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (C) 2011 Eli Golovinsky <eli.golovinsky@gmail.com>.
+# Copyright (C) 2011 Eli Finer <eli.finer@gmail.com>.
 #
 # This file is part of termenu.
 #
@@ -19,19 +19,23 @@
 from distutils.core import setup
 
 DESCRIPTION = """
-Shows an inline interactive menu. Menu items can be supplied as arguments,
-via STDIN (if no arguments were given) or a file (using -f).
-Useful for improving the usability of command line tools such as git.
+Termenu is a command line utility and Python library for displaying console
+based interactive menus.
+
+Traditionally there are two types of applications running the Unix shell: pure
+command line utilities such as grep, sed and awk and full screen interactive
+applications such as Midnight Commander. Termenu aims to bridge this gap and
+allow a modicum of interactivity in regular command line utilities.
 """
 
 setup(
     name='termenu',
-    version='0.3.0',
+    version='1.0.0',
     description='Interactive in-line menus for Unix-based terminals',
     long_description=DESCRIPTION,
-    author='Eli Golovinsky',
+    author='Eli Finer',
     license='GPL',
-    author_email='eli.golovinsky@gmail.com',
+    author_email='eli.finer@gmail.com',
     url='https://github.com/gooli/termenu',
     package_dir={'termenu':'.'},
     packages=['termenu'],
